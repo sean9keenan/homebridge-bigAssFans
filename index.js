72d6ca3fd28b90e0186ccecce66c610f3c6291be
pponce <pponce@users.noreply.github.com>
2017-04-02 07:50:15 -0700

    Update index.js

--- index.js
+++ index.js
@@ -30,14 +30,9 @@ function BigAssFansPlatform(log, config, api) {
   this.fanMaster = new bigAssApi.FanMaster(this.numberOfFans); 
   
   this.fanMaster.onFanFullyUpdated = function(myBigAss){
-    console.log("\nBefore addAccessory:");
-    console.log("Found a new fan with name '" + myBigAss.name + "'");
-    console.log("and identifier: '" + myBigAss.id + "'");
-    console.log("and lightExists: '" + myBigAss.light.exists + "'\n");
     platform.addAccessory(myBigAss);
-    console.log("\nAfter addAccessory:");
     console.log("Found a new fan with name '" + myBigAss.name + "'");
-    console.log("and identifier: '" + myBigAss.id + "'");
+    console.log("and identifier: '" + myBigAss.id + "'\n");
     console.log("and lightExists: '" + myBigAss.light.exists + "'\n");
   }
 
